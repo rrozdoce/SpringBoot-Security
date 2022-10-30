6c583ed18d60a5b8f79075c829bab6558b9334a7
rrozdoce <felipefelipevilhalva@gmail.comm>
2022-10-30 08:51:38 -0400

    done controll-roles ✅

--- src/main/java/com/api/parkingcontrol/configs/security/UserDetailsServiceImpl.java
+++ src/main/java/com/api/parkingcontrol/configs/security/UserDetailsServiceImpl.java
@@ -2,12 +2,15 @@ package com.api.parkingcontrol.configs.security;
 
 import com.api.parkingcontrol.models.UserModel;
 import com.api.parkingcontrol.repositories.UserRepository;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional // se nao colocar da erro na linha 26
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     final UserRepository userRepository;
@@ -22,6 +25,6 @@ public class UserDetailsServiceImpl implements UserDetailsService {
         UserModel userModel = userRepository.findByUsername(username)
                 .orElseThrow( () -> new UsernameNotFoundException("User not Found with username: " + username));
 
-        return userModel;
+        return new User(userModel.getUsername(), userModel.getPassword(), true, true, true, true, userModel.getAuthorities());
     }
 }
